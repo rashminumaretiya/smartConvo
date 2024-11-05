3dd102fb19b3d7ae6d7a6037246d094d561b0c5a
Rashmin Tagline <rsu@taglineinfotech.com>
2024-11-05 17:33:28 +0530

    Responsive changes and animation

--- js/main.js
+++ js/main.js
@@ -1,44 +1,3 @@
-// $(".talking-about-slider").slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 300,
-//     slidesToShow: 1,
-//     adaptiveHeight: true,
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 1,
-//         },
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           slidesToShow: 1,
-//         },
-//       },
-//     ],
-//   });
-
-// $(".owl-carousel").owlCarousel({
-//   loop: true,
-//   margin: 10,
-//   nav: true,
-//   dots: false,
-
-//   responsive: {
-//     0: {
-//       items: 1,
-//     },
-//     600: {
-//       items: 1,
-//     },
-//     1000: {
-//       items: 1,
-//     },
-//   },
-// });
-
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   if (scroll >= 60) {
@@ -53,14 +12,15 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     fade: true,
+    arrows: false,
     autoplay: true,
     autoplaySpeed: 2000,
   });
-  $("#prev").click(function () {
+  $("#testimonial-prev").click(function () {
     $(".talking-about-slider").slick("slickPrev");
   });
 
-  $("#next").click(function () {
+  $("#testimonial-next").click(function () {
     $(".talking-about-slider").slick("slickNext");
   });
   $(".main-slider").slick({
@@ -70,20 +30,69 @@ $(document).ready(function () {
     asNavFor: ".thumbnail-slider",
     autoplay: true,
     autoplaySpeed: 2000,
+    arrows: false,
   });
 
   // Initialize the thumbnail slider
   $(".thumbnail-slider").slick({
     slidesToShow: 3,
+    arrows: false,
     slidesToScroll: 1,
     asNavFor: ".main-slider",
     focusOnSelect: true,
     vertical: true,
     verticalSwiping: true,
     centerMode: true,
-    centerPadding: "98px",
     autoplay: true,
     autoplaySpeed: 2000,
     infinite: true,
+    centerPadding: "98px",
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          centerPadding: "75px",
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          centerPadding: "0px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
+});
+$("#gen-prev").click(function () {
+  $(".thumbnail-slider").slick("slickPrev");
+});
+
+$("#gen-next").click(function () {
+  $(".thumbnail-slider").slick("slickNext");
+});
+
+AOS.init();
+
+// Nav bar Fuctionality
+document.addEventListener("click", function (event) {
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+
+  // Check if the clicked element is outside the navbar collapse and if it's currently open
+  if (
+    !navbarCollapse.contains(event.target) &&
+    navbarCollapse.classList.contains("show")
+  ) {
+    navbarCollapse.classList.remove("show");
+    document.body.style.overflow = ""; // Remove overflow hidden when menu is closed
+  } else if (navbarCollapse.contains(event.target)) {
+    // Check if a link inside the navbar is clicked
+    if (event.target.tagName === "A") {
+      navbarCollapse.classList.remove("show");
+      document.body.style.overflow = ""; // Remove overflow hidden when menu is closed
+    } else {
+      navbarCollapse.classList.add("show");
+      document.body.style.overflow = "hidden"; // Set overflow hidden when menu is open
+    }
+  }
 });
